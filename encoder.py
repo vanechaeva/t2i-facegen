d7be87e9a713bf0996cf1ff6e2f358c7af32b461
vna <mckinleey@yandex.ru>
2024-01-31 21:41:17 +0300

    t2e def added

--- encoder.py
+++ encoder.py
@@ -5,3 +5,13 @@ class SentenceEncoder:
     def __init__(self, device):
         self.bert_model = SentenceTransformer("all-mpnet-base-v2").to(device)
         self.device = device
+    def text_2_embeddings(self, batch_text):
+        stack = []
+        for sent in batch_text:
+            l = sent.split(". ")
+            sentence_embeddings = self.bert_model.encode(l)
+            sentence_emb = torch.FloatTensor(sentence_embeddings).to(self.device)
+            sent_mean = torch.mean(sentence_emb, dim=0).reshape(1, -1)
+            stack.append(sent_mean)
+        output = torch.cat(stack, dim=0)
+        return output.detach()
